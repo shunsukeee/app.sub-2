7bc32837fd776ab7cc2f8be5a48ef1736d97b907
shunsuke <shunsuke.nakayama36@gmail.com>
2020-04-22 23:20:47 +0900

    add

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,14 +1,53 @@
 class BooksController < ApplicationController
-	def new
+	def top
 		
+	end
+  	def new
+		@book = Book.new
 	end
 	def create
-		
+		@book = Book.new(book_params)
+        @book.user_id = current_user.id
+        @book.save
+
+		redirect_to book_path(@book.id)
 	end
+
+
 	def index
 		
 	end
 	def show
-		
+		@user = current_user
+		@book = Book.find(params[:id])
+		@book2 = Book.new
 	end
+	def create
+		@book = Book.new(book_params)
+        @book.user_id = current_user.id
+        @book.save
+
+		redirect_to book_path(@book.id)
+	end
+	def edit
+		@book = Book.find(params[:id])
+	end
+	def update
+		@book = Book.find(params[:id])
+		@book.update(book_params)
+		redirect_to book_path(@book.id)
+	end
+
+	# def destroy
+	# 	book = Book.find(params[id])
+	# 	book.destroy
+	# 	redirect_to book_path
+	# end
+
+
+	private
+	  def book_params
+	  	params.require(:book).permit(:title, :body)
+	  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'books#index'
-  resources :books, only: [:new, :create, :index, :show]
+  root 'books#top'
+  resources :users, :only => [:show]
+  resources :books, :only => [:show, :new, :create, :index, :edit, :update, :create, :destroy]
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
